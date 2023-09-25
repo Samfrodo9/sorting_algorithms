74933ff5b54b41138e1b5a87329ccaabd6b3f175
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-25 18:54:31 +0100

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 		swap = 0;
 		for (j = 0; j < (size - 1 - i); j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j]; /*larger integer*/
 				array[j] = array[j + 1];
